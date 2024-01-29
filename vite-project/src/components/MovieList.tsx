923302f3039628180f5f9b433a190d14104d514b
Vwalln99 <wallner.viktoria@gmx.net>
2024-01-29 09:52:40 +0100

    init commit

--- vite-project/src/components/MovieList.tsx
+++ vite-project/src/components/MovieList.tsx
@@ -1,3 +1,6 @@
+import"../MovieList.css";
+
+
 export default function MovieList(){
     return(
         <div className="container">
@@ -5,21 +8,21 @@ export default function MovieList(){
                 <h2>Title: Killers of the Flower Moon</h2>
                 <h5>Director: Martin Scorcese</h5>
                 <span>Runtime: 3,26 h</span>
-                <div>*****************</div>
+                <div>*****</div>
                 </div>
 
                 <div className="movie-card">
                 <h2>Title: Asteroid City</h2>
                 <h5>Director: Was Anderson</h5>
                 <span>Runtime: 1,45 h</span>
-                <div>*****************</div>
+                <div>****</div>
                 </div>
 
                 <div className="movie-card">
                 <h2>Title: The Whale</h2>
                 <h5>Director: Darren Aranofsky</h5>
                 <span>Runtime: 1,57 h</span>
-                <div>*****************</div>
+                <div>*****</div>
                 </div>
 
         </div>

bec361b6e3cbece10d898a164ea5ecbc3496a9c3
Vwalln99 <wallner.viktoria@gmx.net>
2024-01-29 09:24:18 +0100

    init commit

---
+++ vite-project/src/components/MovieList.tsx
@@ -0,0 +1,27 @@
+export default function MovieList(){
+    return(
+        <div className="container">
+            <div className="movie-card">
+                <h2>Title: Killers of the Flower Moon</h2>
+                <h5>Director: Martin Scorcese</h5>
+                <span>Runtime: 3,26 h</span>
+                <div>*****************</div>
+                </div>
+
+                <div className="movie-card">
+                <h2>Title: Asteroid City</h2>
+                <h5>Director: Was Anderson</h5>
+                <span>Runtime: 1,45 h</span>
+                <div>*****************</div>
+                </div>
+
+                <div className="movie-card">
+                <h2>Title: The Whale</h2>
+                <h5>Director: Darren Aranofsky</h5>
+                <span>Runtime: 1,57 h</span>
+                <div>*****************</div>
+                </div>
+
+        </div>
+    );
+}

7f903660e6633fbcd53026da6cbaffef5be0566b
Vwalln99 <wallner.viktoria@gmx.net>
2024-01-29 13:45:47 +0100

    init commit

--- vite-project/src/components/Formular.tsx
+++ vite-project/src/components/Formular.tsx
@@ -16,5 +16,35 @@ export default function InputForm(){
     const paddingStyle={
         padding: "10px",
     };
-    
+
+    const btnStyle={
+        padding:"10px",
+        backgroundColor: "#add8e6",
+        border:"none",
+        borderRadius:"3px",
+    };
+
+    return(
+        <form>
+            <div style={paddingStyle}>
+                <label style={blockStyle}>Full Name</label>
+                <input type="text" value={userData.fullname}></input>
+            </div>
+            <div style={paddingStyle}>
+                <label style={blockStyle}>E-Mail</label>
+                <input type="email" value={userData.email}></input>
+            </div>
+            <div style={paddingStyle}>
+                <label style={blockStyle}>Password</label>
+                <input type="password" value={userData.password}></input>
+            </div>
+            <div style={paddingStyle}>
+                <label style={blockStyle}>Confirm Password</label>
+                <input type="password" value={userData.confirmPassword}></input>
+            </div>
+            <div style={paddingStyle}>
+                <button type="submit" style={btnStyle}> Submit{" "}</button>
+            </div>
+        </form>
+    );
 }

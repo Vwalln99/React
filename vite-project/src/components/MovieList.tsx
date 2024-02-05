e3b87b3fea7b0677b09eb73727b302b4c5311a17
Vwalln99 <wallner.viktoria@gmx.net>
2024-02-05 08:38:51 +0100

    init commit

--- vite-project/src/components/MovieList.tsx
+++ vite-project/src/components/MovieList.tsx
@@ -8,7 +8,21 @@ export default function MovieList(){
     const [movies, setMovies]=useState<IMovie[]>([]);
     const [err, setErr] = useState<Error | null>(null);
 
-    
+    useEffect(() => {
+        const connect = async() =>{
+            try{
+                const data = await fetch("http://localhost:5000/movies");
+                if (!data.ok){
+                    throw new Error("Couldn't connect to server!");
+                }
+                setMovies((await data.json()) as IMovie[]);
+            } catch (error){
+                setErr(error as Error);
+            }
+        };
+        connect();
+    }, []);
+
     const handleRating=(id:number, rating: number):void =>{
         setMovies((prevMovie) => {
             return prevMovie.filter((movie) =>{
@@ -17,14 +31,26 @@ export default function MovieList(){
             });
         });
     };
-    return(
-        <div className="container">
-          {movies.map((movie):JSX.Element=>{
-            return <MovieListItem key={movie.id}
-             movie={movie}
-             onRating={handleRating}/>;
-          })}  
+    
+    {
+        if(err !== null){
+            return <div>{err?.message}</div>;
+        } else{
+            return(
+                <div className="container">
+                    {" "}
+                  {movies.map((movie):JSX.Element => {
+                    return (
+                        <MovieListItem 
+                            key={movie.id}
+                            movie={movie}
+                            onRating={handleRating}
+                        />
+                    );
+                })}  
+            </div>
+            );
+        }
+    }
 
-        </div>
-    );
 }
